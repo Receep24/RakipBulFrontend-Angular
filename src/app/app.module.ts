acfa6c85abde8b8535aead8779e57b1c579ab601
Recep ÖZKARA <97729329+Receep24@users.noreply.github.com>
2023-09-21 10:29:59 +0300

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
