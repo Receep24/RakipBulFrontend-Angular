2de06dece3a901d663e4bed45cb50f52e25125e8
Recep ÖZKARA <97729329+Receep24@users.noreply.github.com>
2023-09-25 11:47:38 +0300

    app component düzenleme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
-<<<<<<< HEAD
+
 import { SignComponent } from './sign/sign.component';
-=======
+
 import { CardComponent } from './card/card.component';
 import { ContentComponent } from './content/content.component';
 import { IlanComponent } from './ilan/ilan.component';
 import { RouterModule } from '@angular/router';
->>>>>>> 3c997239c9ba14e0c85e0e4a8a78d47eb73d9adc
+
 
 @NgModule({
   declarations: [
@@ -20,14 +20,11 @@ import { RouterModule } from '@angular/router';
     NavbarComponent,
     FooterComponent,
     BannerComponent,
-<<<<<<< HEAD
-    SignComponent
-=======
+    SignComponent,
     CardComponent,
     ContentComponent,
     IlanComponent,
 
->>>>>>> 3c997239c9ba14e0c85e0e4a8a78d47eb73d9adc
   ],
   imports: [
     BrowserModule,
