f1d8997d9b09991b9d1881c4fffaef562f051c49
Recep ÖZKARA <97729329+Receep24@users.noreply.github.com>
2023-09-26 14:27:19 +0300

    admin panel giydirme

---
+++ src/assets/vendor/chart.js/helpers/helpers.color.d.ts
@@ -0,0 +1,6 @@
+
+export declare function isPatternOrGradient(value: unknown): value is CanvasPattern | CanvasGradient;
+
+export declare function getHoverColor(value: CanvasGradient): CanvasGradient;
+export declare function getHoverColor(value: CanvasPattern): CanvasPattern;
+export declare function getHoverColor(value: string): string;
